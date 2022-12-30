3eb14ad6911e69cd92648d19714d54ddfd9ce849
Will <montero_w@live.com>
2022-12-29 18:31:25 -0600

    Created some testing assets and created containers for the abilities

--- Assets/Scripts/Will/AbilityContainer.cs
+++ Assets/Scripts/Will/AbilityContainer.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class AbilityContainer : MonoBehaviour
 {
+    private AbilityContainer _abilityOne;
+    private AbilityContainer _abilityTwo;
+    private AbilityContainer _abilityThree;
+
     public AbilityContainer[] abilityContainers;
+
+
+
+    void Restarting()
+    {
+        _abilityOne = new AbilityContainer();
+        _abilityTwo = new AbilityContainer();
+        _abilityThree = new AbilityContainer();
+    }
 }
--- Assets/Scripts/Will/AttackStats.cs
+++ Assets/Scripts/Will/AttackStats.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Ability", menuName = "Ability")]
 public class AttackStats : ScriptableObject
 {
+    //The name of this ability 
+    [Tooltip("The name of this ability ")]
     public string AbilityName = "defult";
+
+    [Tooltip("For short discrptive discription of the ability maybe even lore")]
     public string AbilityDiscription = "None";
+
+    [Tooltip("Ability physical")]
+    public GameObject AbilityArt;
+
+    [Tooltip("Current ability damage out put")]
     public float AbilityDamage = 0.0f;
 }
